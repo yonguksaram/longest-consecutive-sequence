c0d14a3a2eebb0930b3dde921622fe313c4fe19e
Stanislau Alimpau <alimberg@list.ru>
2018-03-03 20:20:31 +0300

    longest-consecutive-sequence/feature: adding code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports =    function longestConsecutiveLength(array) {
+  var newArray = array.concat();
+  newArray.sort(function compareNumeric(a, b) {
+return a - b;
+});
+  var count = 1;
+  var lengthOfSequence = 0;
+  for (var i=0;i<newArray.length;i++){
+    if(newArray[i+1]==newArray[i]) {
+      continue;
+    }
+    if(newArray[i+1]-newArray[i]==1) {
+      count++;
+    } else {
+      if(count>lengthOfSequence){
+        lengthOfSequence = count;
+      }
+        count = 1;
+      
+    }
+  }
+return lengthOfSequence;
 }
